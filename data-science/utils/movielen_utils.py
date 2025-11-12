280fab8b4e65f125536f5a8266ce688847968034
tung leduc <chopperman259@gmail.com>
2025-11-13 06:45:55 +0700

    update

--- data-science/utils/movielen_utils.py
+++ data-science/utils/movielen_utils.py
@@ -3,6 +3,14 @@ import urllib
 import zipfile
 import os
 
+
+
+
+
+
+class MoviLenLinkResource:
+    ML100K = "http://files.grouplens.org/datasets/movielens/ml-100k.zip"
+
 def download_ml100k(dest_folder_name: str):
 
     dest_file_path = os.path.join("../datasets/movielen_datasets",dest_folder_name)
@@ -10,9 +18,9 @@ def download_ml100k(dest_folder_name: str):
     zip_ref = zipfile.ZipFile('movielens.zip', "r")
     zip_ref.extractall()
 
-def read_u_base_to_df(local_file: str):
+def read_u_base_to_df(ml100k_path: str):
     import pandas as pd
     column_names = ["user_id", "movie_id", "rating", "timestamp"]
+    df = pd.read_csv(os.path.join(ml100k_path, "u.base"), names=column_names, sep = "\t", encoding="latin-1")
 
-    df = pd.read_csv(local_file, names=column_names, sep = "\t", encoding="latin-1")
-
+    return df
