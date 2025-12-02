6fb433d0cb6e22516b5e4c33e25361d6f152ccff
tung leduc <chopperman259@gmail.com>
2025-12-03 00:12:58 +0700

    try signup ui

--- client/web/web-rec/src/app/app.component.ts
+++ client/web/web-rec/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignupComponent } from './account/signup/signup.component';
+import { SigninComponent } from "./account/signin/signin.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SignupComponent, SigninComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
