2c944519869789340514262837e17bf1936a2bcd
tung leduc <chopperman259@gmail.com>
2025-12-03 00:10:46 +0700

    update abstraction

--- backend/api/modules/bases/supports/abstraction.py
+++ backend/api/modules/bases/supports/abstraction.py
@@ -1,61 +1,76 @@
 from abc import ABC, abstractmethod
 from uuid import UUID
 
-from pydantic import BaseModel as DtoModel
-from typing import Dict, Any, List, Generator, Tuple
+from pydantic import BaseModel
+from typing import Dict, Any, List, Generator, Tuple, Sequence, TypeVar, Optional, Generic
+
+from sqlmodel import SQLModel
+
+TModel = TypeVar('TModel', bound=BaseModel)
+TEntity = TypeVar('TEntity', bound=SQLModel)
+
+class ISupportRepository(ABC, Generic[TEntity,TModel]):
+
+    @abstractmethod
+    async def exist_record(self, idx: UUID) -> bool:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def find_by_id(self, idx: UUID) -> TModel:
+        raise NotImplementedError()
 
 
-class ISupportRepository(ABC):
-
-	@abstractmethod
-	async def exist_record(self, idx: UUID) -> bool:
-		raise NotImplementedError()
+    @abstractmethod
+    async def find_by_field(self, field_name: str, value: Any) -> List[TModel]:
+        raise NotImplementedError()
 
 
-	@abstractmethod
-	def find_by_id(self, idx: UUID) -> DtoModel:
-		raise NotImplementedError()
+    @abstractmethod
+    async def find_all(self) -> List[TModel]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def find_all_by_condition(self, condition: Dict[str, Any]) -> Sequence[TModel] | Generator[TModel, None, None]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def update(self, model: TModel, refresh_data = False) -> Tuple[bool, Optional[TModel]]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def partial_update(self, model: TModel, update_properties: List[str],
+                             refresh_data: bool = False) -> Tuple[bool, Optional[TModel]]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def query_raw(self, query_question: str, parameters: Dict[str, Any]) -> List[TModel]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def query_raw_take_first(self, query_question: str, parameters: Dict[str, Any]) -> TModel:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def delete(self, model: TModel, ) -> bool:
+        raise NotImplementedError()
+
+    @abstractmethod
+    async def delete_by_id(self, idx: UUID) -> TModel:
+        raise NotImplementedError()
 
 
-	@abstractmethod
-	async def find_by_field(self, field_name: str, value: Any) -> List[DtoModel]:
-		raise NotImplementedError()
+from enum import Enum
+
+class RestrictAccessType(str, Enum):
+    ReadOnly = 1
+    Writable = 2
+    Readable = 3
+    Constant = 3
+    RequireRole = 4
+    AuthWritable = 5
 
 
-	@abstractmethod
-	async def find_all(self) -> List[DtoModel]:
-		raise NotImplementedError()
-
-
-	@abstractmethod
-	async def find_all_by_condition(self, condition: Dict[str, Any]) -> List[DtoModel] | Generator[DtoModel, None, None]:
-		raise NotImplementedError()
-
-	@abstractmethod
-	async def update(self, model: DtoModel) -> Tuple[bool, DtoModel]:
-		raise NotImplementedError()
-
-	@abstractmethod
-	async def partial_update(self, model: DtoModel, update_properties: List[str]) -> Tuple[bool, DtoModel]:
-		raise NotImplementedError()
-
-	@abstractmethod
-	async def query_raw(self, query_question: str, parameters: Dict[str, Any]) -> List[DtoModel]:
-		raise NotImplementedError()
-
-	@abstractmethod
-	async def query_raw_take_first(self, query_question: str, parameters: Dict[str, Any]) -> DtoModel:
-		raise NotImplementedError( )
-
-	@abstractmethod
-	async def delete(self, model: DtoModel, soft_delete: bool = False) -> bool:
-		raise NotImplementedError()
-
-
-	async def delete_by_id(self, idx: UUID) -> Tuple[bool, DtoModel]:
-		raise NotImplementedError()
-
-	"""mapping-fields from model to columns name of database"""
-	mapping_fields: Dict[str, Any]
-
-
+class RestrictActionType(str, Enum):
+    NoPublic = 1
+    Private = 2
+    Public = 3
